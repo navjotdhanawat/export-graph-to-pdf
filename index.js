67b13be568d7a6d4a62d01d14a996d0b1d007974
Dhanawat <Navjotsing.Dhanawat@int.asurion.com>
2019-02-27 20:23:33 +0530

    Initial commit : export d3js graph to PDF

---
+++ index.js
@@ -0,0 +1,58 @@
+const D3Node = require('d3-node')
+
+const options = { selector: '#chart', container: '<div id="container"><div id="chart"></div></div>' }
+const d3n = new D3Node(options)
+const d3 = d3n.d3
+
+
+
+// set the dimensions and margins of the graph
+var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the line
+var valueline = d3.line()
+    .x(function (d) { return x(d.year); })
+    .y(function (d) { return y(d.population); });
+
+let svg = d3.select(d3n.document.querySelector('#chart')).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g").attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+
+// format the data
+let data = [{ "year": "2006", "population": "40" }, { "year": "2008", "population": "45" }, { "year": "2010", "population": "48" }, { "year": "2012", "population": "51" }, { "year": "2014", "population": "53" }, { "year": "2016", "population": "57" }, { "year": "2017", "population": "62" }];
+data.forEach(function (d) {
+    d.year = d.year;
+    d.population = +d.population;
+});
+
+// Scale the range of the data
+x.domain(d3.extent(data, function (d) { return d.year; }));
+y.domain([0, d3.max(data, function (d) { return d.population; })]);
+
+// Add the valueline path.
+svg.append("path")
+    .data([data])
+    .attr("class", "line")
+    .attr("d", valueline);
+
+// Add the X Axis
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// Add the Y Axis
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+let svgStr = d3n.svgString()
+
+console.log(svgStr)
